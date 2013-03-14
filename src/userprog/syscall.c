616473c1367e6dc82ac02c0a536027f9cb5cc9c0
RyanDS <rdspring1@gmail.com>
2013-03-13 23:27:55 -0500

    Implemented User Memory Access

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,10 +1,32 @@
 #include "userprog/syscall.h"
+#include "userprog/pagedir.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+
+/* Get Kernel Physical Address from
+   User Virtual Address; otherwise
+   return null pointer*/
+static uint8_t *
+convert_uvaddr_kpaddr (const uint8_t *uaddr)
+{
+  	if(uaddr != null)
+	{
+  		if(is_user_vaddr((void*) uaddr))
+		{
+         void* kvaddr = pagedir_get_page(active_pd(), uaddr);
+		   if(kvadd != null)
+		   {
+			   return (uint8_t *) vtop(kvaddr);
+		   }
+      }
+  	} 
+  	return null;
+}
 
 void
 syscall_init (void) 

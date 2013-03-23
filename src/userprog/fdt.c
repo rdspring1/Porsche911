1aba43e930b6a957a68c8208d8bedde601f23b9d
rdspring <rdspring1@gmail.com>
2013-03-22 23:15:44 -0500

    Implement File System Call

--- src/userprog/fdt.c
+++ src/userprog/fdt.c
@@ -3,6 +3,7 @@
 #include "threads/malloc.h"
 #include "threads/thread.h"
 #include "userprog/fdt.h"
+#include <stdio.h>
 
 /* Slots 0 and 1 in the array of struct file * should always be
    reserved for stdin and stdout. I'm unsure of how to actually
@@ -26,10 +27,12 @@ int fd_create(struct file *file)
 
   // initialized to 2 "leave space" for stdin and stdout
   for (i = 2; i < FDT_MAX_FILES; i++)
-    if (fdt[i] != NULL) {
-      fdt[i] = file;
-      return i;
-    }
+  {
+    	if (fdt[i] == 0) {
+			fdt[i] = file;
+			return i;
+		}
+  }
 
   return -1; // no room
 }
@@ -49,25 +52,26 @@ struct file *fd_get_file(int fd)
    responsibility. */
 struct file *fd_remove(int fd)
 {
-  ASSERT(0 <= fd && fd < FDT_MAX_FILES);
+  if(0 <= fd && fd < FDT_MAX_FILES)
+	  return NULL;
 
   fdt_t fdt = thread_current()->fdt;
   struct file *file = fdt[fd];
 
-  fdt[fd] = NULL;
+  fdt[fd] = 0;
   return file;
 }
 
 /* Closes all files (except stdin and stdout) and frees memory. */
 void fdt_destroy(fdt_t fdt)
 {
-  if (fdt == NULL)
+  if (fdt == 0)
     return;
 
   int i;
 
   for (i = 2; i < FDT_MAX_FILES; i++)
-    if (fdt[i] != NULL)
+    if (fdt[i] != 0)
       file_close(fdt[i]);
 
   free(fdt);
@@ -77,5 +81,5 @@ void fdt_destroy(fdt_t fdt)
    making a slight assumption that NULL is indeed 0. */
 fdt_t fdt_init()
 {
-  return (fdt_t) calloc(FDT_MAX_FILES, sizeof(struct file *));
+  	return (fdt_t) calloc(FDT_MAX_FILES, sizeof(struct file *));
 }
